98d7405f3aa0ab1b04077bef9ec5ba02ce7db07e
Vivek Lakum <lakumvivek01@gmail.com>
2024-09-24 19:46:46 +0530

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,84 +1,100 @@
 import java.util.Scanner;
 
-class Process {
-    int pid;
-    int AT;
-    int BT;
-    int rBT;
-    int WT;
-    int TAT;
-    int CT;
-    int lastExecTime;
-}
+public class RoundRobinScheduling {
 
-public class RoundRobin {
-    public static void main(String[] args) {
+    public static void roundRobinScheduling() {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the number of processes: ");
-        int n = sc.nextInt();
-
-        System.out.println("Enter the time quantum: ");
-        int TQ = sc.nextInt();
-
-        Process[] proc = new Process[n];
-        int currTime = 0, completed = 0;
-        double avgWT = 0, avgTAT = 0, avgCT = 0;
-
-        for (int i = 0; i < n; i++) {
-            proc[i] = new Process();
-
-           
-            System.out.print("Process ID: ");
-            proc[i].pid = sc.nextInt();
-
-            System.out.print("Arrival Time: ");
-            proc[i].AT = sc.nextInt();
-
-            System.out.print("Burst Time: ");
-            proc[i].BT = sc.nextInt();
-
-            proc[i].rBT = proc[i].BT;
-            proc[i].WT = 0;
-            proc[i].lastExecTime = proc[i].AT;
+        
+        String[] processNames = new String[20];
+        int[] arrivalTimes = new int[20];
+        int[] burstTimes = new int[20];
+        int[] completionTimes = new int[20];
+        int[] tempBurstTimes = new int[20];
+        int[] waitingTimes = new int[20];
+        int[] turnaroundTimes = new int[20];
+        
+        int totalWaitingTime = 0;
+        int totalTurnaroundTime = 0;
+        int totalCompletionTime = 0;
+        
+        System.out.print("Enter number of processes: ");
+        int numProcesses = sc.nextInt();
+        sc.nextLine(); 
+        
+        System.out.println("Enter the process names: ");
+        for (int i = 0; i < numProcesses; i++) {
+            processNames[i] = sc.nextLine();
         }
-
-        while (completed < n) {
-            boolean processExecuted = false;
-            for (int i = 0; i < n; i++) {
-                if (proc[i].rBT == 0 || proc[i].AT > currTime) continue;
-                else if (proc[i].rBT <= TQ) {
-                    currTime += proc[i].rBT;
-                    proc[i].rBT = 0;
-                    proc[i].CT = currTime;
-                    proc[i].TAT = proc[i].CT - proc[i].AT;
-                    completed++;
-                    processExecuted = true;
-                } else {
-                    currTime += TQ;
-                    proc[i].rBT -= TQ;
-                    processExecuted = true;
+        
+        System.out.println("Enter arrival times: ");
+        for (int i = 0; i < numProcesses; i++) {
+            arrivalTimes[i] = sc.nextInt();
+        }
+        
+        System.out.println("Enter CPU burst times: ");
+        for (int i = 0; i < numProcesses; i++) {
+            burstTimes[i] = sc.nextInt();
+            tempBurstTimes[i] = burstTimes[i]; 
+        }
+        
+        System.out.print("Enter quantum time: ");
+        int quantumTime = sc.nextInt();
+        
+        int currentTime = 0;
+        int completedProcesses = 0;
+        
+        while (completedProcesses < numProcesses) {
+            boolean done = true;
+            for (int i = 0; i < numProcesses; i++) {
+                if (tempBurstTimes[i] > 0) {
+                    done = false; 
+                    if (tempBurstTimes[i] > quantumTime) {
+                        currentTime += quantumTime;
+                        tempBurstTimes[i] -= quantumTime;
+                    } else {
+                        currentTime += tempBurstTimes[i];
+                        completionTimes[i] = currentTime;
+                        tempBurstTimes[i] = 0;
+                        completedProcesses++;
+                    }
                 }
-                proc[i].lastExecTime = currTime;
             }
-            if (!processExecuted) break;
+            if (done) {
+                break;
+            }
         }
-
-        for (int i = 0; i < n; i++) {
-            proc[i].WT = proc[i].TAT - proc[i].BT;
-            avgTAT += proc[i].TAT;
-            avgWT += proc[i].WT;
-            avgCT += proc[i].CT;
+        
+        for (int i = 0; i < numProcesses; i++) {
+            waitingTimes[i] = completionTimes[i] - arrivalTimes[i] - burstTimes[i];
+            totalWaitingTime += waitingTimes[i];
+            turnaroundTimes[i] = completionTimes[i] - arrivalTimes[i];
+            totalTurnaroundTime += turnaroundTimes[i];
+            totalCompletionTime += completionTimes[i];
         }
+        
+        double averageWaitingTime = (double) totalWaitingTime / numProcesses;
+        double averageTurnaroundTime = (double) totalTurnaroundTime / numProcesses;
+        double averageCompletionTime = (double) totalCompletionTime / numProcesses;
+        
+        System.out.println("PID\tAT\tBT\tCT\tTAT\tWT");
+        for (int i = 0; i < numProcesses; i++) {
+            System.out.println(processNames[i] + "\t" + arrivalTimes[i] + "\t" + burstTimes[i] + "\t" +
+                               completionTimes[i] + "\t" + turnaroundTimes[i] + "\t" + waitingTimes[i]);
+        }
+        
+        System.out.printf("Total completion time = %.2f\n", (double) totalCompletionTime);
+        System.out.printf("Total turn around time = %.2f\n", (double) totalTurnaroundTime);
+        System.out.printf("Total waiting time = %.2f\n", (double) totalWaitingTime);
+        System.out.printf("Average completion time: %.2f\n", averageCompletionTime);
+        System.out.printf("Average turn around time: %.2f\n", averageTurnaroundTime);
+        System.out.printf("Average waiting time: %.2f\n", averageWaitingTime);
+        System.out.println("\n" + "=".repeat(40) + "\n");  
+    }
 
-        avgWT /= n;
-        avgTAT /= n;
-        avgCT /= n;
-
-        System.out.printf("Average Completion Time: %.2f\n", avgCT);
-        System.out.printf("Average Turnaround Time: %.2f\n", avgTAT);
-        System.out.printf("Average Waiting Time: %.2f\n", avgWT);
-
-        sc.close();
+    public static void main(String[] args) {
+  
+        for (int i = 0; i < 3; i++) {
+            roundRobinScheduling();
+        }
     }
 }
